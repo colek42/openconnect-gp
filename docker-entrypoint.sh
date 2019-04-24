014e04628650e16fb116cd2cb6ecbe1dc15fe6a9
Cole Kennedy <colek42@gmail.com>
2019-04-24 14:02:56 -0500

    fix entrypoint and tokenize vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo $PASSWORD | openconnect --user=$USER --passwd-on-stdin --protocol=gp --servercert pin-sha256:Z/TjaX+kI7y2b0OtApdOcJKfWtqFycfKCgbZ3d7OKhw= --certificate=/certs/cert.pem cosbppan01.keysight.com
+echo $PASSWORD | openconnect --user=${USER} --passwd-on-stdin --protocol=gp --certificate=/certs/cert.pem --servercert pin-sha256:$CERTPIN $SERVER
+
